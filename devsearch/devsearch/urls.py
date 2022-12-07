3446292ab8662dd5bebdde36c2eb8dd67301698c
Neil Allwood <neilallwood@icloud.com>
2022-12-07 15:38:39 +0000

    Updated urls.py with projects and root paths

--- devsearch/devsearch/urls.py
+++ devsearch/devsearch/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def projects(request):
+    return HttpResponse("Here are the products")
 
 urlpatterns = [
+    path('', projects, name = "root"),
     path('admin/', admin.site.urls),
+    path('projects/', projects, name = "projects")
 ]
