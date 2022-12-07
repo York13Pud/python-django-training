2f6fb666836c1b59a5244603250ebc077be2fe69
Neil Allwood <neilallwood@icloud.com>
2022-12-07 18:23:24 +0000

    Moved views from main urls.py to this views.py

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+
+def projects(request):
+    return HttpResponse("Here are the products")
+
+
+def project(request, key):
+    return HttpResponse(f"The key is {key}")
