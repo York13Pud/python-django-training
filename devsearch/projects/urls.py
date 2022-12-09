0c0568aca7254bb56913c44c1d3341c67a48ac54
Neil Allwood <neilallwood@icloud.com>
2022-12-09 12:46:07 +0000

    Changed projects url to be root

--- devsearch/projects/urls.py
+++ devsearch/projects/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('projects/', views.projects, name = "projects"),
+    path('', views.projects, name = "projects"),
     path('project/<str:key>/', views.project, name = "project")
 ]
