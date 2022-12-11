d74f5cd49533b38ae24e13f8b76ed2c26834c9c4
Neil Allwood <neilallwood@icloud.com>
2022-12-11 16:40:51 +0000

    Added method to show title instead of row obj name

--- devsearch/projects/models.py
+++ devsearch/projects/models.py
@@ -3,6 +3,8 @@ import uuid
 
 # Create your models here.
 class Project(models.Model):
+    """ Create a simple table to add some example data into.
+    """
     id = models.UUIDField(primary_key = True, 
                           default = uuid.uuid4, 
                           unique = True, 
@@ -17,3 +19,12 @@ class Project(models.Model):
                                    null = True, 
                                    blank = True)
     created = models.DateTimeField(auto_now_add = True)
+    
+    
+    def __str__(self):
+        """_summary_
+            This returns a string representation of the title in the admin panel for a row, rather than the object description.
+        Returns:
+            This returns a string representation of the title in the admin panel for a row, rather than the object description.
+        """
+        return self.title
