521a81f1f3dab0b80c759cf623624acd2f248737
Neil Allwood <neilallwood@icloud.com>
2022-12-13 15:45:33 +0000

    Added more models to admin panel

--- devsearch/projects/admin.py
+++ devsearch/projects/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Project
+from .models import Project, Review, Tag
 
 # Register the models here.
 admin.site.register(model_or_iterable = Project)
+admin.site.register(model_or_iterable = Review)
+admin.site.register(model_or_iterable = Tag)
