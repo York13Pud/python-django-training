b69de68a68a472547e8fa3de5f5c386292031280
Neil Allwood <neilallwood@icloud.com>
2022-12-16 10:16:46 +0000

    Added urlpattern for create-project view

--- devsearch/projects/urls.py
+++ devsearch/projects/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.projects, name = "projects"),
-    path('project/<str:key>/', views.project, name = "project")
+    path('project/<str:key>/', views.project, name = "project"),
+    path('create-project/', views.create_project, name = "create-project")
 ]
