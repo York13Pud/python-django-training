cf3ac9ae665340ae903f10d39b2f3896a8bc8896
Neil Allwood <neilallwood@icloud.com>
2022-12-19 10:54:04 +0000

    Added update-post url to the list

--- devsearch/projects/urls.py
+++ devsearch/projects/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.projects, name = "projects"),
     path('project/<str:key>/', views.project, name = "project"),
-    path('create-project/', views.create_project, name = "create-project")
+    path('create-project/', views.create_project, name = "create-project"),
+    path('update-project/<str:key>/', views.update_project, name = "update-project")
 ]
