3d17c5793627b825ced96110622286b70b57ecde
Neil Allwood <neilallwood@icloud.com>
2022-12-19 13:14:58 +0000

    Added delete-project url

--- devsearch/projects/urls.py
+++ devsearch/projects/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.projects, name = "projects"),
     path('project/<str:key>/', views.project, name = "project"),
     path('create-project/', views.create_project, name = "create-project"),
-    path('update-project/<str:key>/', views.update_project, name = "update-project")
+    path('update-project/<str:key>/', views.update_project, name = "update-project"),
+    path('delete-project/<str:key>/', views.delete_project, name = "delete-project")
 ]
