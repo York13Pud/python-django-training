9ae166007920750f09cbb3ca4cb13cb90960cee4
Neil Allwood <neilallwood@icloud.com>
2022-12-21 17:01:53 +0000

    Added STATIC_ROOT, ALLOWED_HOSTS & disabled DEBUG

--- devsearch/devsearch/settings.py
+++ devsearch/devsearch/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@dkta@%qe+2ba3f^49ekxf++--cwfd7a&k_-1i()o^&tzjj-lf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
