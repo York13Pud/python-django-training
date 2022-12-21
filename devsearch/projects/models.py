f1945ac5a6111e84855bcccef1e9012cf31a0220
Neil Allwood <neilallwood@icloud.com>
2022-12-21 10:27:00 +0000

    Added featured_image to project model

--- devsearch/projects/models.py
+++ devsearch/projects/models.py
@@ -12,6 +12,9 @@ class Project(models.Model):
     title = models.CharField(max_length = 200)
     description = models.TextField(null=True, 
                                    blank = True)
+    featured_image = models.ImageField(null = True,
+                                       blank = True,
+                                       default = "default.jpg")
     demo_link = models.CharField(max_length = 2000, 
                                  null = True, 
                                  blank = True)
