710703a586e5ab4319e81e457fbfe4654f9b08dd
Neil Allwood <neilallwood@icloud.com>
2022-12-24 21:11:46 +0000

    Enabled debug mode

--- devsearch/devsearch/settings.py
+++ devsearch/devsearch/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@dkta@%qe+2ba3f^49ekxf++--cwfd7a&k_-1i()o^&tzjj-lf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
