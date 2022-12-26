1324bf957d890ca9d47236e71ec8efcd668b69df
Neil Allwood <neilallwood@icloud.com>
2022-12-26 17:00:14 +0000

    Set form inputs to classes

--- devsearch/projects/forms.py
+++ devsearch/projects/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 
 
@@ -6,3 +7,10 @@ class Project_Form(ModelForm):
     class Meta:
         model = Project
         fields = ["title", "description", "featured_image", "demo_link", "source_link", "tag"]
+        widgets = {"tag": forms.CheckboxSelectMultiple()}
+    
+    def __init__(self, *args, **kwargs):
+        super(Project_Form, self).__init__(*args, **kwargs)
+        
+        for name, field in self.fields.items():
+            field.widget.attrs.update({"class": "input"})
