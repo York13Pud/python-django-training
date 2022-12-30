4c26fe8a674088db95dd2ae550b4c9f64bf2ab91
Neil Allwood <neilallwood@icloud.com>
2022-12-30 14:29:52 +0000

    Added temporary user-profile view, url & template

--- devsearch/users/urls.py
+++ devsearch/users/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.profiles, name = "profiles"),
+    path('profile', views.profile, name = "user-profile"),
 ]
--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -6,3 +6,7 @@ def profiles(request):
     profiles = Profile.objects.all()
     context = {"profiles": profiles}
     return render(request, "users/profiles.html", context = context)
+
+
+def profile(request):
+    return render(request, "users/profile.html")
