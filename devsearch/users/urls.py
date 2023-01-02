0ecee8679f82106d4f065da8159f606bf3463307
Neil Allwood <neilallwood@icloud.com>
2023-01-02 17:10:58 +0000

    Added user-profile URL for a single user profile

--- devsearch/users/urls.py
+++ devsearch/users/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.profiles, name = "profiles"),
-    path('profile', views.profile, name = "user-profile"),
+    path('user-profile/<str:pk>', views.user_profile, name = "user-profile"),
 ]
