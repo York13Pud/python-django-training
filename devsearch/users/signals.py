e1de98d9d06b18926f1e689f515fe3fa0f43fd9c
Neil Allwood <neilallwood@icloud.com>
2023-01-03 15:47:19 +0000

    Created a dedicated file for all users app signals

---
+++ devsearch/users/signals.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from .models import Profile
+
+
+
+# @receiver(post_save, sender = Profile)
+def create_profile(sender, instance, created, **kwargs):
+    """If the account is a new user, create the user in the main 
+    Django users database and create a profile for that user"""
+    
+    if created:
+        user = instance
+        profile = Profile.objects.create(user = user, 
+                                         username = user.username, 
+                                         email = user.email, 
+                                         name = user.first_name)
+
+
+post_save.connect(create_profile, sender = User)
+
+
+# @receiver(post_delete)
+def delete_profile(sender, instance, **kwargs):
+    """If the user profile is deleted, this function will also delete 
+    the user account that is stored in the main Django users database."""
+    
+    user = instance.user
+    user.delete()
+
+
+post_delete.connect(delete_profile, sender = Profile)
