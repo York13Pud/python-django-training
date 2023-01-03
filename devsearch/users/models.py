e0afce59f217ef512352b875549cfb325cf2163b
Neil Allwood <neilallwood@icloud.com>
2023-01-03 13:54:20 +0000

    Added signal to delete user if profile is deleted

--- devsearch/users/models.py
+++ devsearch/users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 
 import uuid
 
@@ -87,21 +88,30 @@ class Skill(models.Model):
         """
         return str(self.name)
 
+
+# @receiver(post_save, sender = Profile)
+def create_profile(sender, instance, created, **kwargs):
+    """If the account is a new user, create the user in the main 
+    Django users database and create a profile for that user"""
     
-def profile_updated(sender, instance, created, **kwargs):
-    print("Profile Saved")
-    print(f"Instance: {instance}")
-    print(f"Created: {created}")
-
-   
-post_save.connect(profile_updated, 
-                  sender = Profile)
+    if created:
+        user = instance
+        profile = Profile.objects.create(user = user, 
+                                         username = user.username, 
+                                         email = user.email, 
+                                         name = user.first_name)
 
 
-def profile_deleted(sender, instance, **kwargs):
-    print("Profile Deleted")
-    print(f"Instance: {instance}")
+post_save.connect(create_profile, sender = User)
 
 
-post_delete.connect(profile_deleted, 
-                    sender = Profile)
+# @receiver(post_delete)
+def delete_profile(sender, instance, **kwargs):
+    """If the user profile is deleted, this function will also delete 
+    the user account that is stored in the main Django users database."""
+    
+    user = instance.user
+    user.delete()
+
+
+post_delete.connect(delete_profile, sender = Profile)
