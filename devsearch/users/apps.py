c0d30597f8d0c27e33c1f8e52da64bf46f88607f
Neil Allwood <neilallwood@icloud.com>
2023-01-03 15:47:50 +0000

    Registered signals file with users apps.py file

--- devsearch/users/apps.py
+++ devsearch/users/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+    
+    def ready(self) -> None:
+        """Import the signals file. Without this, the signals file is ignored."""
+        import users.signals
