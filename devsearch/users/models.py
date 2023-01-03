9458fc85de4c0eed21be24dcb3fcce39bd5e4ba7
Neil Allwood <neilallwood@icloud.com>
2023-01-03 15:46:46 +0000

    Removed signals and unused imports from file

--- devsearch/users/models.py
+++ devsearch/users/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save, post_delete
-from django.dispatch import receiver
+
 
 import uuid
 
@@ -87,31 +86,3 @@ class Skill(models.Model):
             This returns a string representation of the title in the admin panel for a row, rather than the object description.
         """
         return str(self.name)
-
-
-# @receiver(post_save, sender = Profile)
-def create_profile(sender, instance, created, **kwargs):
-    """If the account is a new user, create the user in the main 
-    Django users database and create a profile for that user"""
-    
-    if created:
-        user = instance
-        profile = Profile.objects.create(user = user, 
-                                         username = user.username, 
-                                         email = user.email, 
-                                         name = user.first_name)
-
-
-post_save.connect(create_profile, sender = User)
-
-
-# @receiver(post_delete)
-def delete_profile(sender, instance, **kwargs):
-    """If the user profile is deleted, this function will also delete 
-    the user account that is stored in the main Django users database."""
-    
-    user = instance.user
-    user.delete()
-
-
-post_delete.connect(delete_profile, sender = Profile)
