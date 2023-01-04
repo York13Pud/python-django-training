ddb4fb63036d17b3024291442587d083577d40d4
Neil Allwood <neilallwood@icloud.com>
2023-01-04 12:56:26 +0000

    Changed login_page url to login_user

--- devsearch/users/urls.py
+++ devsearch/users/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.profiles, name = "profiles"),
     path('user-profile/<str:pk>/', views.user_profile, name = "user-profile"),
-    path('login/', views.login_page, name = "login"),
+    path('login/', views.login_user, name = "login"),
     path('logout/', views.logout_user, name = "logout"),
 ]
