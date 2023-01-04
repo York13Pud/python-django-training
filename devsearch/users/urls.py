4ccac35179f86f8f3a48fea9f0ddc7f677dd2ebb
Neil Allwood <neilallwood@icloud.com>
2023-01-04 12:45:28 +0000

    Added url for login

--- devsearch/users/urls.py
+++ devsearch/users/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.profiles, name = "profiles"),
-    path('user-profile/<str:pk>', views.user_profile, name = "user-profile"),
+    path('user-profile/<str:pk>/', views.user_profile, name = "user-profile"),
+    path('login/', views.login_page, name = "login"),
 ]
