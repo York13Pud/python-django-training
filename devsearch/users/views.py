6fbcd86e5a4d9696dfa7ec0ae16f0c74e353cfb0
Neil Allwood <neilallwood@icloud.com>
2023-01-04 12:45:42 +0000

    Added view for login

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
 from .models import Profile
+
+
 # Create your views here.
+
+
+def login_page(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        
+        try:
+            user = User.objects.get(username = username)
+        except:
+            print("Username does not exist")
+        
+        user = authenticate(request = request,
+                            username = username,
+                            password = password)
+        
+        if user is not None:
+            login(request, user = user)
+            return redirect("profiles")
+        else:
+            print("Username or password is incorrect")
+    
+    return render(request, "users/login_register.html")
+
 
 def profiles(request):
     profiles = Profile.objects.all()
