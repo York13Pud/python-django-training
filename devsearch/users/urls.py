10bd76ea9f96d5961aa7a19fdb8ee861ae2d68a2
Neil Allwood <neilallwood@icloud.com>
2023-01-05 14:36:11 +0000

    Added URL for registering a user

--- devsearch/users/urls.py
+++ devsearch/users/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.profiles, name = "profiles"),
-    path('user-profile/<str:pk>/', views.user_profile, name = "user-profile"),
-    path('login/', views.login_user, name = "login"),
-    path('logout/', views.logout_user, name = "logout"),
+    path("", views.profiles, name = "profiles"),
+    path("user-profile/<str:pk>/", views.user_profile, name = "user-profile"),
+    path("login/", views.login_user, name = "login"),
+    path("logout/", views.logout_user, name = "logout"),
+    path("register/", views.register_user, name = "register"),
 ]
