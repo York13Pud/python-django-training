96508a3ea6ac3e502786dc93ccf931e6cd8b513b
Neil Allwood <neilallwood@icloud.com>
2023-01-06 16:08:57 +0000

    Added a method to close flash alert with a button

--- devsearch/static/uikit/app.js
+++ devsearch/static/uikit/app.js
@@ -2,3 +2,22 @@
 document.addEventListener('DOMContentLoaded', function () {
   hljs.highlightAll();
 });
+
+let alertWrapper = document.querySelector('.alert')
+let alertClose = document.querySelector('.alert__close')
+
+if (alertWrapper) {
+  alertClose.addEventListener('click', () =>
+    alertWrapper.style.display = 'none'
+  )
+}
+
+// if (alertWrapper) {
+//   alertClose.addEventListener("click", () => (alertWrapper.style.display = "none"));
+// }
+
+// function closeButton() {
+//   alertWrapper.style.display = "none"
+// }
+
+// alertClose.addEventListener("click", closeButton) 
