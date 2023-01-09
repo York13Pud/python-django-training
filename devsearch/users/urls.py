e40c156f6b7d07b2627058a922298d3add69f4c1
Neil Allwood <neilallwood@icloud.com>
2023-01-09 09:59:20 +0000

    Added URL's for account management & adding skills

--- devsearch/users/urls.py
+++ devsearch/users/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path("login/", views.login_user, name = "login"),
     path("logout/", views.logout_user, name = "logout"),
     path("register/", views.register_user, name = "register"),
+    path("account/", views.user_account, name = "account"),
+    path("edit-account/", views.edit_account, name = "edit-account"),
+    path("create-skill/", views.create_skill, name = "create-skill"),
 ]
