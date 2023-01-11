30b5048c25f22dfe425f1462c243db8fb58deafa
Neil Allwood <neilallwood@icloud.com>
2023-01-11 11:28:37 +0000

    Added signal that updates a users profile

--- devsearch/users/signals.py
+++ devsearch/users/signals.py
@@ -31,3 +31,17 @@ def delete_profile(sender, instance, **kwargs):
 
 
 post_delete.connect(delete_profile, sender = Profile)
+
+
+def update_user(sender, instance, created, **kwargs):
+    profile = instance
+    user = profile.user
+
+    if created == False:
+        user.first_name = profile.name
+        user.username = profile.username
+        user.email = profile.email
+        user.save()
+        
+
+post_save.connect(update_user, sender=Profile)
